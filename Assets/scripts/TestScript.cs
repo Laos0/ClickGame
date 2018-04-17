c4b14ee604d3ab893faec42bad093314c140ad78
C-EC8EB5A41BCA\Student <isonylao@gmail.com>
2018-04-16 19:39:42 -0500

    Added in blocks, interact with them, get sound

--- Assets/scripts/TestScript.cs
+++ Assets/scripts/TestScript.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class TestScript : MonoBehaviour {
-
+	
 	// Use this for initialization
+	public AudioSource audio;
+
 	void Start () {
 		
 	}
@@ -12,5 +15,11 @@ public class TestScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnMouseDown(){
+		//Debug.Log ("Clicked");
+		audio.Play ();
+
 	}
 }
