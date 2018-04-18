fdc47a85a3c019d95a9ddc9eaa14155b38c5c742
SkrzypchakL <traversal96@gmail.com>
2018-04-18 09:52:04 -0500

    Added puzzle folder, basic starting assets and scripts for puzzle.

---
+++ Assets/objects/puzzle/scripts/blkCount.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blkCount : MonoBehaviour {
+
+	// Use this for initialization
+
+	public int count;
+
+	void Start () {
+		count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown(){
+		count++;
+	}
+}
---
+++ Assets/objects/puzzle/scripts/blkText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blkText : MonoBehaviour {
+
+	// Use this for initialization
+
+	public int textCount;
+	public GameObject blkCount;
+
+	void Start () {
+		textCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Get count from blkCount
+		textCount = blkCount.GetComponent().count;
+	}
+}
