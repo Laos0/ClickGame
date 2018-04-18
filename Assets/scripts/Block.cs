598515f30d7d44c4367a0ff260f06dde33677e52
Nicholas Reynolds <reynoldsn6924@my.uwstout.edu>
2018-04-18 11:28:15 -0500

    Added block prototype

---
+++ Assets/scripts/Block.cs
@@ -0,0 +1,28 @@
+﻿// This class is the parent class for all Block objects
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    // The number of clicks needed to break this block
+    protected int clicksNeeded;
+    // The number of points the player gets for breaking this block
+    protected int reward;
+
+    // Use this for initialization
+    void Start () {
+        clicksNeeded = 20;
+        reward = 50;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Getters
+    public int getReward() { return reward; }
+    public int getClicksNeeded() { return clicksNeeded; }
+}
