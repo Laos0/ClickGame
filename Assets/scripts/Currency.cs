1cf329b593d8a8bc2893f52f9d15525711ede832
Nicholas Reynolds <reynoldsn6924@my.uwstout.edu>
2018-04-18 09:37:09 -0500

    Added functions to currency.cs

--- Assets/scripts/Currency.cs
+++ Assets/scripts/Currency.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 
 public class Currency : MonoBehaviour {
 
-	// Use this for initialization
+	
+    // UI text object to display the player's currency
 	public Text scoreTxt;
+    // Amount of currency the player has
 	public int counter;
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
+        // Initialize the player's currency to zero
 		counter = 0;
 	}
 	
@@ -16,4 +21,42 @@ public class Currency : MonoBehaviour {
 	void Update () {
 		scoreTxt.text = "" + counter;
 	}
+
+
+    // Give the player more money
+    public void addMoney(int amount)
+    {
+        counter += amount;
+    }
+
+    // Attempt to spend amount money
+    // Returns true if the player had enough money for the transaction
+    public bool spendMoney(int amount)
+    {
+        // If player has enough money
+        if (counter >= amount)
+        {
+            // Take the player's money
+            counter -= amount;
+            return true;
+        }
+
+        // Return false if they couldn't afford it
+        return false;
+    }
+
+
+    // Checks whether the player has at least amount money, without making the player spend it
+    public bool hasAtLeast(int amount)
+    {
+        // If player has enough money
+        if (counter >= amount)
+        {
+            return true;
+        }
+
+        // Return false if they don't have enough
+        return false;
+    }
+
 }
