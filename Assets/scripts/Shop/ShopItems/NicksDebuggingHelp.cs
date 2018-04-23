342019b8277961f3b2b63122156db4d3cfeda128
Nicholas Reynolds <reynoldsn6924@my.uwstout.edu>
2018-04-23 09:35:34 -0500

    Updated Shop System

---
+++ Assets/scripts/Shop/ShopItems/NicksDebuggingHelp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NicksDebuggingHelp : MonoBehaviour {
+
+    public UnityEngine.UI.Text moneyDisplay;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moneyDisplay.text = Currency.counter.ToString();
+	}
+}
