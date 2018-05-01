d12af990389a95abbc965bfc96f8792050e32d38
Nicholas Reynolds <reynoldsn6924@my.uwstout.edu>
2018-05-01 18:08:17 -0500

    Added a new script for button functions

---
+++ Assets/scripts/Shop/OpenShop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenShop : MonoBehaviour {
+
+    public GameObject shopOb;
+    
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void hideShop()
+    {
+        shopOb.SetActive(false);
+    }
+
+    public void showShop()
+    {
+        shopOb.SetActive(true);
+    }
+}
