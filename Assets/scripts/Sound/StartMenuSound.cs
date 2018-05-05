88dbc9e5d5a32e411d0180ac44862cf5d6b8ee1a
C-EC8EB5A41BCA\Student <isonylao@gmail.com>
2018-05-05 16:05:04 -0500

    added in StartMenuSound

---
+++ Assets/scripts/Sound/StartMenuSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenuSound : MonoBehaviour {
+
+    public AudioSource buttonAudio;
+
+
+    // Use this for initialization
+    void Start () {
+
+        DontDestroyOnLoad(this);
+
+	}
+
+    public void playStartMenuButtonSound()
+    {
+        if (buttonAudio != null)
+        {
+            buttonAudio.Play();
+        }
+        else
+        {
+            Debug.Log("buttonAudio is null");
+        }
+    }
+}
